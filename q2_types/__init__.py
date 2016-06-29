ef9333e217966faf4816678a69d3dcd77c91b742
Greg Caporaso <gregcaporaso@gmail.com>
2016-06-29 15:08:41 -0700

    ENH: fixes #8

--- q2_types/__init__.py
+++ q2_types/__init__.py
@@ -11,8 +11,10 @@ __version__ = "0.0.0-dev"  # noqa
 from ._feature_table import (FeatureTable, Frequency, RelativeFrequency,
                              PresenceAbsence)
 from ._distance_matrix import DistanceMatrix
+from ._alpha_diversity import AlphaDiversity
 from ._tree import Phylogeny
 from ._ordination import PCoAResults
 
 __all__ = ['DistanceMatrix', 'Phylogeny', 'PCoAResults', 'FeatureTable',
-           'Frequency', 'RelativeFrequency', 'PresenceAbsence']
+           'Frequency', 'RelativeFrequency', 'PresenceAbsence',
+           'AlphaDiversity']
