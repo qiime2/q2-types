51033731e0bb1f0cd0d868e782a9359d604ce677
Jorden <jakereps@users.noreply.github.com>
2016-09-26 11:10:34 -0700

    TST: FeatureData Type Tests

--- q2_types/feature_data/tests/test_type.py
+++ q2_types/feature_data/tests/test_type.py
@@ -5,3 +5,53 @@
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
+
+import unittest
+
+from q2_types.feature_data import (
+    FeatureData, Taxonomy, Sequence, PairedEndSequence, AlignedSequence,
+    TaxonomyDirectoryFormat, DNASequencesDirectoryFormat,
+    PairedDNASequencesDirectoryFormat, AlignedDNASequencesDirectoryFormat
+)
+from q2_types.testing import TestPluginBase
+
+
+class TestTypes(TestPluginBase):
+    def test_feature_data_semantic_type_registration(self):
+        self.assertRegisteredSemanticType(FeatureData)
+
+    def test_taxonomy_semantic_type_registration(self):
+        self.assertRegisteredSemanticType(Taxonomy)
+
+    def test_sequence_semantic_type_registration(self):
+        self.assertRegisteredSemanticType(Sequence)
+
+    def test_paired_end_sequence_semantic_type_registration(self):
+        self.assertRegisteredSemanticType(PairedEndSequence)
+
+    def test_aligned_sequence_semantic_type_registration(self):
+        self.assertRegisteredSemanticType(AlignedSequence)
+
+    def test_taxonomy_semantic_type_to_format_registration(self):
+        self.assertSemanticTypeRegisteredToFormat(
+                FeatureData[Taxonomy], TaxonomyDirectoryFormat)
+
+    def test_sequence_semantic_type_to_format_registration(self):
+        self.assertSemanticTypeRegisteredToFormat(
+                FeatureData[Sequence], DNASequencesDirectoryFormat)
+
+    def test_paired_end_sequence_semantic_type_to_format_registration(self):
+        self.assertSemanticTypeRegisteredToFormat(
+                FeatureData[PairedEndSequence],
+                PairedDNASequencesDirectoryFormat
+        )
+
+    def test_aligned_sequence_semantic_type_to_format_registration(self):
+        self.assertSemanticTypeRegisteredToFormat(
+                FeatureData[AlignedSequence],
+                AlignedDNASequencesDirectoryFormat
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
