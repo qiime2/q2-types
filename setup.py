692cabac5dd1266ef96f148f93569e25e430b0b2
Matthew Dillon <matthewrdillon@gmail.com>
2016-11-07 14:18:37 -0700

    MAINT: Bump version to 0.0.7.dev0

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ from setuptools import setup, find_packages
 setup(
     name="q2-types",
     # TODO stop duplicating version string
-    version="0.0.7.dev",
+    version="0.0.7.dev0",
     packages=find_packages(),
-    install_requires=['scikit-bio', 'qiime >= 2.0.7', 'pandas',
+    install_requires=['scikit-bio', 'qiime >= 2.0.6', 'pandas',
                       'biom-format >= 2.1.5, < 2.2.0', 'ijson',
                       'h5py'],
     author="Greg Caporaso",
