d3f87f03e6ba3fc7d9b1637b2d4bc4542e0d2294
Jorden <jakereps@users.noreply.github.com>
2016-11-30 16:58:20 -0700

    TST: Per Sample Sequences Format Tests

---
+++ q2_types/per_sample_sequences/tests/test_format.py
@@ -0,0 +1,131 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016--, QIIME development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+import shutil
+import unittest
+
+from q2_types.per_sample_sequences import (
+    CasavaOneEightSingleLanePerSampleDirFmt, FastqGzFormat, YamlFormat,
+    FastqManifestFormat, SingleLanePerSampleSingleEndFastqDirFmt,
+    SingleLanePerSamplePairedEndFastqDirFmt
+)
+from qiime.plugin.testing import TestPluginBase
+
+
+class TestFormats(TestPluginBase):
+    package = 'q2_types.per_sample_sequences.tests'
+
+    def test_fastq_gz_format_validate_positive(self):
+        filepath = self.get_data_path('Human-Kneecap_S1_L001_R1_001.fastq.gz')
+        format = FastqGzFormat(filepath, mode='r')
+
+        format.validate()
+
+    def test_fastq_gz_format_validate_negative(self):
+        filepath = self.get_data_path('not-fastq.fastq.gz')
+        format = FastqGzFormat(filepath, mode='r')
+
+        with self.assertRaisesRegex(ValueError, 'FastqGzFormat'):
+            format.validate()
+
+    def test_yaml_format_validate_positive(self):
+        filepath = self.get_data_path('metadata.yml')
+        format = YamlFormat(filepath, mode='r')
+
+        format.validate()
+
+    def test_yaml_format_validate_negative(self):
+        filepath = self.get_data_path('not-metadata.yml')
+        format = YamlFormat(filepath, mode='r')
+
+        with self.assertRaisesRegex(ValueError, 'YamlFormat'):
+            format.validate()
+
+    def test_fastq_manifest_format_validate_positive(self):
+        filepath = self.get_data_path('MANIFEST')
+        format = FastqManifestFormat(filepath, mode='r')
+
+        format.validate()
+
+    def test_fastq_manifest_format_validate_negative(self):
+        filepath = self.get_data_path('not-MANIFEST')
+        format = FastqManifestFormat(filepath, mode='r')
+
+        with self.assertRaisesRegex(ValueError, 'FastqManifestFormat'):
+            format.validate()
+
+    def test_casava_one_eight_slanepsample_dir_fmt_validate_positive(self):
+        filepath = self.get_data_path('Human-Kneecap_S1_L001_R1_001.fastq.gz')
+        shutil.copy(filepath, self.temp_dir.name)
+
+        format = CasavaOneEightSingleLanePerSampleDirFmt(
+            self.temp_dir.name, mode='r')
+
+        format.validate()
+
+    def test_casava_one_eight_slanepsample_dir_fmt_validate_negative(self):
+        filepath = self.get_data_path('not-fastq.fastq.gz')
+        shutil.copy(filepath, self.temp_dir.name)
+
+        format = CasavaOneEightSingleLanePerSampleDirFmt(
+            self.temp_dir.name, mode='r')
+
+        with self.assertRaisesRegex(ValueError, 'CasavaOneEightSingleLanePer'):
+            format.validate()
+
+    def test_slanepsample_single_end_fastq_dir_fmt_validate_positive(self):
+        filenames = ('MANIFEST', 'metadata.yml',
+                     'Human-Kneecap_S1_L001_R1_001.fastq.gz')
+        for filename in filenames:
+            filepath = self.get_data_path(filename)
+            shutil.copy(filepath, self.temp_dir.name)
+
+        format = SingleLanePerSampleSingleEndFastqDirFmt(
+            self.temp_dir.name, mode='r')
+
+        format.validate()
+
+    def test_slanepsample_single_end_fastq_dir_fmt_validate_negative(self):
+        filenames = ('MANIFEST', 'metadata.yml', 'not-fastq.fastq.gz')
+        for filename in filenames:
+            filepath = self.get_data_path(filename)
+            shutil.copy(filepath, self.temp_dir.name)
+
+        format = SingleLanePerSampleSingleEndFastqDirFmt(
+            self.temp_dir.name, mode='r')
+
+        with self.assertRaisesRegex(ValueError, 'SingleLanePerSampleSingle'):
+            format.validate()
+
+    def test_slanepsample_paired_end_fastq_dir_fmt_validate_positive(self):
+        filenames = ('MANIFEST', 'metadata.yml',
+                     'Human-Kneecap_S1_L001_R1_001.fastq.gz')
+        for filename in filenames:
+            filepath = self.get_data_path(filename)
+            shutil.copy(filepath, self.temp_dir.name)
+
+        format = SingleLanePerSamplePairedEndFastqDirFmt(
+            self.temp_dir.name, mode='r')
+
+        format.validate()
+
+    def test_slanepsample_paired_end_fastq_dir_fmt_validate_negative(self):
+        filenames = ('MANIFEST', 'metadata.yml', 'not-fastq.fastq.gz')
+        for filename in filenames:
+            filepath = self.get_data_path(filename)
+            shutil.copy(filepath, self.temp_dir.name)
+
+        format = SingleLanePerSamplePairedEndFastqDirFmt(
+            self.temp_dir.name, mode='r')
+
+        with self.assertRaisesRegex(ValueError, 'SingleLanePerSamplePaired'):
+            format.validate()
+
+
+if __name__ == "__main__":
+    unittest.main()
