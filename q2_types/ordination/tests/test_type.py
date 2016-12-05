825a73f52ec7e8da7e501c44a4d594fe52559b01
Jorden Kreps <jakereps@users.noreply.github.com>
2016-12-05 08:34:26 -0700

    TST: Add format, transformer, and type tests to Ordination (#80)

---
+++ q2_types/ordination/tests/test_type.py
@@ -0,0 +1,27 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016--, QIIME development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+import unittest
+
+from q2_types.ordination import PCoAResults, OrdinationDirectoryFormat
+from qiime.plugin.testing import TestPluginBase
+
+
+class TestTypes(TestPluginBase):
+    package = 'q2_types.ordination.tests'
+
+    def test_pcoa_results_semantic_type_registration(self):
+        self.assertRegisteredSemanticType(PCoAResults)
+
+    def test_pcoa_results_semantic_type_to_ordination_fmt_registration(self):
+        self.assertSemanticTypeRegisteredToFormat(PCoAResults,
+                                                  OrdinationDirectoryFormat)
+
+
+if __name__ == "__main__":
+    unittest.main()
