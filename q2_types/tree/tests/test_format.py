07fbd78b3973fea11db15004abbfcc3e2dbbc4f1
Jorden <jakereps@users.noreply.github.com>
2016-12-07 13:42:40 -0700

    TST: Tree Format Tests

---
+++ q2_types/tree/tests/test_format.py
@@ -0,0 +1,41 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016--, QIIME development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+import shutil
+import unittest
+
+from q2_types.tree import NewickFormat, NewickDirectoryFormat
+from qiime.plugin.testing import TestPluginBase
+
+
+class TestFormats(TestPluginBase):
+    package = "q2_types.tree.tests"
+
+    def test_newick_format_validate_positive(self):
+        filepath = self.get_data_path('tree.nwk')
+        format = NewickFormat(filepath, mode='r')
+
+        format.validate()
+
+    def test_newick_format_validate_negative(self):
+        filepath = self.get_data_path('not-tree.nwk')
+        format = NewickFormat(filepath, mode='r')
+
+        with self.assertRaisesRegex(ValueError, 'NewickFormat'):
+            format.validate()
+
+    def test_newick_directory_format_validate_postivie(self):
+        filepath = self.get_data_path('tree.nwk')
+        shutil.copy(filepath, self.temp_dir.name)
+        format = NewickDirectoryFormat(self.temp_dir.name, mode='r')
+
+        format.validate()
+
+
+if __name__ == '__main__':
+    unittest.main()
