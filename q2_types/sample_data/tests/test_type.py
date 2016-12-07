1a14d790080f803520b3c9f0bafb15bb426977a3
Jorden <jakereps@users.noreply.github.com>
2016-12-07 13:33:56 -0700

    TST: SampleData Type Tests

---
+++ q2_types/sample_data/tests/test_type.py
@@ -0,0 +1,31 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016--, QIIME development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+import unittest
+
+from q2_types.sample_data import (AlphaDiversityDirectoryFormat,
+                                  SampleData, AlphaDiversity)
+from qiime.plugin.testing import TestPluginBase
+
+
+class TestTypes(TestPluginBase):
+    package = "q2_types.sample_data.tests"
+
+    def test_sample_data_semantic_type_registration(self):
+        self.assertRegisteredSemanticType(SampleData)
+
+    def test_alpha_diversity_semantic_type_registration(self):
+        self.assertRegisteredSemanticType(AlphaDiversity)
+
+    def test_sample_data_alpha_div_to_alpha_div_dir_fmt_registration(self):
+        self.assertSemanticTypeRegisteredToFormat(
+            SampleData[AlphaDiversity], AlphaDiversityDirectoryFormat)
+
+
+if __name__ == '__main__':
+    unittest.main()
