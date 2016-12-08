9d9d1d978811899f28f2776ee34d9316abcf5db4
Jorden Kreps <jakereps@users.noreply.github.com>
2016-12-08 13:50:00 -0700

    TST: Add format, transformer, and type tests to Sample Data (#86)

---
+++ q2_types/sample_data/tests/test_format.py
@@ -0,0 +1,42 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016--, QIIME development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+import shutil
+import unittest
+
+from q2_types.sample_data import (AlphaDiversityDirectoryFormat,
+                                  AlphaDiversityFormat)
+from qiime.plugin.testing import TestPluginBase
+
+
+class TestFormats(TestPluginBase):
+    package = "q2_types.sample_data.tests"
+
+    def test_alpha_diversity_format_validate_positive(self):
+        filepath = self.get_data_path('alpha-diversity.tsv')
+        format = AlphaDiversityFormat(filepath, mode='r')
+
+        format.validate()
+
+    def test_alpha_diversity_format_validate_negative(self):
+        filepath = self.get_data_path('not-alpha-diversity.tsv')
+        format = AlphaDiversityFormat(filepath, mode='r')
+
+        with self.assertRaisesRegex(ValueError, 'AlphaDiversityFormat'):
+            format.validate()
+
+    def test_alpha_diversity_dir_fmt_validate_positive(self):
+        filepath = self.get_data_path('alpha-diversity.tsv')
+        shutil.copy(filepath, self.temp_dir.name)
+        format = AlphaDiversityDirectoryFormat(self.temp_dir.name, mode='r')
+
+        format.validate()
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ q2_types/sample_data/tests/test_transformer.py
@@ -0,0 +1,41 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016--, QIIME development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+import unittest
+
+import pandas as pd
+
+from pandas.util.testing import assert_series_equal
+from q2_types.sample_data import AlphaDiversityFormat
+from qiime.plugin.testing import TestPluginBase
+
+
+class TestTransformers(TestPluginBase):
+    package = "q2_types.sample_data.tests"
+
+    def test_pd_series_to_alpha_diversity_format(self):
+        filename = self.get_data_path('alpha-diversity.tsv')
+        transformer = self.get_transformer(pd.Series, AlphaDiversityFormat)
+        input = pd.Series.from_csv(filename, sep='\t', header=0)
+
+        obs = transformer(input)
+        obs = pd.Series.from_csv(str(obs), sep='\t', header=0)
+
+        assert_series_equal(input, obs)
+
+    def test_alpha_diversity_format_to_pd_series(self):
+        filename = 'alpha-diversity.tsv'
+        input, obs = self.transform_format(AlphaDiversityFormat, pd.Series,
+                                           filename)
+        input = pd.Series.from_csv(str(input), sep='\t', header=0)
+
+        assert_series_equal(input, obs)
+
+
+if __name__ == '__main__':
+    unittest.main()
