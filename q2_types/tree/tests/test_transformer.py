346e8680833665958dba035a0fc7b6fd4953d592
Jorden Kreps <jakereps@users.noreply.github.com>
2016-12-08 13:52:24 -0700

    TST: Add format, transformer, and type tests to Tree (#87)

---
+++ q2_types/tree/tests/test_transformer.py
@@ -0,0 +1,41 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016--, QIIME development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+import unittest
+
+import skbio
+
+from q2_types.tree import NewickFormat
+from qiime.plugin.testing import TestPluginBase
+
+
+class TestTransformers(TestPluginBase):
+    package = "q2_types.tree.tests"
+
+    def test_skbio_tree_node_to_newick_format(self):
+        filepath = self.get_data_path('tree.nwk')
+        transformer = self.get_transformer(skbio.TreeNode, NewickFormat)
+        input = skbio.TreeNode.read(filepath)
+
+        obs = transformer(input)
+        obs = skbio.TreeNode.read(str(obs))
+
+        self.assertEqual(str(input), str(obs))
+
+    def test_newick_format_to_skbio_tree_node(self):
+        filename = 'tree.nwk'
+        input, obs = self.transform_format(NewickFormat, skbio.TreeNode,
+                                           filename)
+
+        exp = skbio.TreeNode.read(str(input))
+
+        self.assertEqual(str(exp), str(obs))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ q2_types/tree/tests/test_type.py
@@ -0,0 +1,33 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016--, QIIME development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+import unittest
+
+from q2_types.tree import Phylogeny, Rooted, Unrooted, NewickDirectoryFormat
+from qiime.plugin.testing import TestPluginBase
+
+
+class TestTypes(TestPluginBase):
+    package = "q2_types.tree.tests"
+
+    def test_phylogeny_semantic_type_registration(self):
+        self.assertRegisteredSemanticType(Phylogeny)
+
+    def test_rooted_semantic_type_registration(self):
+        self.assertRegisteredSemanticType(Rooted)
+
+    def test_unrooted_semantic_type_registration(self):
+        self.assertRegisteredSemanticType(Unrooted)
+
+    def test_phylogeny_rooted_unrooted_to_newick_dir_fmt_registration(self):
+        self.assertSemanticTypeRegisteredToFormat(
+            Phylogeny[Rooted | Unrooted], NewickDirectoryFormat)
+
+
+if __name__ == '__main__':
+    unittest.main()
