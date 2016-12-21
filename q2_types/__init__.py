e689491387784543d1fa299565262087c10c5471
Matthew Dillon <matthewrdillon@gmail.com>
2016-12-21 10:04:15 -0700

    MAINT: Drop top-level exports

--- q2_types/__init__.py
+++ q2_types/__init__.py
@@ -5,30 +5,18 @@
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
+
+import importlib
 import pkg_resources
 
 __version__ = pkg_resources.get_distribution('q2-types').version
 
-# TODO remove all imports and from __all__ because they are available as public
-# imports in their respective subpackages. These imports are here so the
-# existing tests will pass; they can be removed when the tests are rewritten.
 
-from .feature_table import (FeatureTable, Frequency, RelativeFrequency,
-                            PresenceAbsence)
-from .distance_matrix import DistanceMatrix
-from .sample_data import SampleData, AlphaDiversity
-from .tree import Phylogeny, Rooted, Unrooted
-from .ordination import PCoAResults
-from .feature_data import (FeatureData, Taxonomy, Sequence, PairedEndSequence,
-                           AlignedSequence, DNAIterator, PairedDNAIterator)
-from .reference_features import ReferenceFeatures, SSU
-from .per_sample_sequences import (SequencesWithQuality,
-                                   PairedEndSequencesWithQuality)
-
-__all__ = ['DistanceMatrix', 'Phylogeny', 'PCoAResults', 'FeatureTable',
-           'Frequency', 'RelativeFrequency', 'PresenceAbsence',
-           'SampleData', 'AlphaDiversity', 'FeatureData', 'Taxonomy',
-           'Sequence', 'PairedEndSequence', 'AlignedSequence',
-           'ReferenceFeatures', 'SSU', 'Rooted', 'Unrooted', 'DNAIterator',
-           'PairedDNAIterator', 'SequencesWithQuality',
-           'PairedEndSequencesWithQuality']
+importlib.import_module('q2_types.feature_table')
+importlib.import_module('q2_types.distance_matrix')
+importlib.import_module('q2_types.tree')
+importlib.import_module('q2_types.ordination')
+importlib.import_module('q2_types.sample_data')
+importlib.import_module('q2_types.feature_data')
+importlib.import_module('q2_types.reference_features')
+importlib.import_module('q2_types.per_sample_sequences')
