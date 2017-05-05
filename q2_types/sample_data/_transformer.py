fac4c3291f3fab28a263dd757bc267a5f25895fe
Jamie Morton <jamietmorton@gmail.com>
2017-05-05 09:45:23 -0700

    ENH: Adding transformer from sample data to metadata

--- q2_types/sample_data/_transformer.py
+++ q2_types/sample_data/_transformer.py
@@ -10,6 +10,7 @@ import pandas as pd
 
 from ..plugin_setup import plugin
 from . import AlphaDiversityFormat
+from qiime2 import Metadata
 
 
 @plugin.register_transformer
@@ -27,3 +28,13 @@ def _2(ff: AlphaDiversityFormat) -> pd.Series:
         # docs recommend using from_csv here (rather than the more commonly
         # used pd.read_csv).
         return pd.Series.from_csv(fh, sep='\t', header=0)
+
+
+@plugin.register_transformer
+def _3(ff: AlphaDiversityFormat) -> Metadata:
+    with ff.open() as fh:
+        # Since we're wanting to round-trip with pd.Series.to_csv, the pandas
+        # docs recommend using from_csv here (rather than the more commonly
+        # used pd.read_csv).
+        df = pd.DataFrame.from_csv(fh, sep='\t', header=0)
+        return Metadata(df)
