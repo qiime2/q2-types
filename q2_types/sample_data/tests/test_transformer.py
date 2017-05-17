767c6ccd8557a8eb6af78acdc5cbf380ca96e04d
Matthew Dillon <matthewrdillon@gmail.com>
2017-05-17 16:36:49 -0700

    BUG: AlphaDiversityFormat-> pd.Series index dtype fix (#116)
    Alpha diversity index is now read as a string and alpha diversity values are read as floats. This solves issues when the index is composed of ints or floats and they need to be read as strings.
    
    Fixes qiime2/q2-diversity#91
    Fixes qiime2/qiime2#258

--- q2_types/sample_data/tests/test_transformer.py
+++ q2_types/sample_data/tests/test_transformer.py
@@ -19,22 +19,38 @@ class TestTransformers(TestPluginBase):
     package = "q2_types.sample_data.tests"
 
     def test_pd_series_to_alpha_diversity_format(self):
-        filename = self.get_data_path('alpha-diversity.tsv')
         transformer = self.get_transformer(pd.Series, AlphaDiversityFormat)
-        input = pd.Series.from_csv(filename, sep='\t', header=0)
+        exp_index = pd.Index(['Sample1', 'Sample4'], dtype=object)
+        exp = pd.Series([0.970950594455, 0.721928094887],
+                        name='shannon', index=exp_index)
 
-        obs = transformer(input)
+        obs = transformer(exp)
         obs = pd.Series.from_csv(str(obs), sep='\t', header=0)
 
-        assert_series_equal(input, obs)
+        assert_series_equal(exp, obs)
 
     def test_alpha_diversity_format_to_pd_series(self):
         filename = 'alpha-diversity.tsv'
-        input, obs = self.transform_format(AlphaDiversityFormat, pd.Series,
-                                           filename)
-        input = pd.Series.from_csv(str(input), sep='\t', header=0)
+        _, obs = self.transform_format(AlphaDiversityFormat, pd.Series,
+                                       filename)
+        exp_index = pd.Index(['Sample1', 'Sample4'], dtype=object)
+        exp = pd.Series([0.970950594455, 0.721928094887],
+                        name='shannon', index=exp_index)
+        assert_series_equal(exp, obs)
 
-        assert_series_equal(input, obs)
+    def test_alpha_diversity_format_to_pd_series_int_indices(self):
+        filename = 'alpha-diversity-int-indices.tsv'
+        _, obs = self.transform_format(AlphaDiversityFormat, pd.Series,
+                                       filename)
+
+        exp_index = pd.Index(['1', '4'], dtype=object)
+        exp = pd.Series([0.97, 0.72], name='foo', index=exp_index)
+        assert_series_equal(exp, obs)
+
+    def test_non_alpha_diversity(self):
+        filename = 'also-not-alpha-diversity.tsv'
+        with self.assertRaisesRegex(ValueError, 'Unable to parse string'):
+            self.transform_format(AlphaDiversityFormat, pd.Series, filename)
 
 
 if __name__ == '__main__':
