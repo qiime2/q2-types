e8089789aa389d49122b0c653ac9e488c574bbe7
David Rodriguez <davidrod805@gmail.com>
2019-08-06 09:11:46 -0700

    SQUASH: Stripped 'Taxon' column in transformer and edited test files

--- q2_types/feature_data/_transformer.py
+++ q2_types/feature_data/_transformer.py
@@ -168,6 +168,7 @@ def _6(ff: TaxonomyFormat) -> pd.Series:
 @plugin.register_transformer
 def _28(ff: TaxonomyFormat) -> qiime2.Metadata:
     df = _taxonomy_formats_to_dataframe(str(ff), has_header=None)
+    df['Taxon'] = df['Taxon'].str.strip()
     return qiime2.Metadata(df)
 
 
@@ -191,8 +192,7 @@ def _23(ff: TSVTaxonomyFormat) -> pd.Series:
 @plugin.register_transformer
 def _29(ff: TSVTaxonomyFormat) -> qiime2.Metadata:
     df = _taxonomy_formats_to_dataframe(str(ff), has_header=True)
-    for index in range(len(df['Taxon'])):
-        df['Taxon'][index] = df['Taxon'][index].strip()
+    df['Taxon'] = df['Taxon'].str.strip()
     return qiime2.Metadata(df)
 
 
