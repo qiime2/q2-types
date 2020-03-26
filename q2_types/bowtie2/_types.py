004403d689f0aa62c54f5041078883773600e304
Nicholas Bokulich <nbokulich@gmail.com>
2020-03-26 13:28:53 -0700

    NEW: bowtie2 index type/formats (#243)

---
+++ q2_types/bowtie2/_types.py
@@ -0,0 +1,20 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2020, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from qiime2.plugin import SemanticType
+from . import Bowtie2IndexDirFmt
+from ..plugin_setup import plugin
+
+
+# Technically there is a bit more to this, for instance the ref sequences may
+# or may not be present in an index, or may be the only thing in an index,
+# but let's not worry about that just yet.
+Bowtie2Index = SemanticType('Bowtie2Index')
+
+plugin.register_semantic_types(Bowtie2Index)
+plugin.register_semantic_type_to_format(Bowtie2Index, Bowtie2IndexDirFmt)
