fae48fca633efe8531fa8972bb72d2f7cb16f54b
nbokulich <nbokulich@gmail.com>
2020-03-26 11:39:08 -0700

    ported over bowtie2 types from q2-shogun

---
+++ q2_types/bowtie2/__init__.py
@@ -0,0 +1,16 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2020, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from ._formats import (Bowtie2IndexFileFormat, Bowtie2IndexDirFmt)
+from ._types import Bowtie2Index
+
+
+__version__ = get_versions()['version']
+del get_versions
+
+__all__ = ['Bowtie2IndexFileFormat', 'Bowtie2IndexDirFmt', 'Bowtie2Index']
