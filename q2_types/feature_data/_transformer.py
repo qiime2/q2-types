298f43096c3bd851c1e79aece7820a6d2ba61393
Greg Caporaso <gregcaporaso@gmail.com>
2020-06-15 08:31:37 -0700

    lint error fixed, squash

--- q2_types/feature_data/_transformer.py
+++ q2_types/feature_data/_transformer.py
@@ -378,6 +378,7 @@ def _35(data: pd.Series) -> AlignedDNAFASTAFormat:
             skbio.io.write(sequence, format='fasta', into=f)
     return ff
 
+
 @plugin.register_transformer
 def _36(fmt: AlignedDNAFASTAFormat) -> DNAIterator:
     generator = _read_dna_fasta(str(fmt))
