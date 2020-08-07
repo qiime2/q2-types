cab3b1a615e0d13d9e5e9a51f505f668bc051238
Leo-Simpson <52931376+Leo-Simpson@users.noreply.github.com>
2020-08-07 18:04:56 +0200

    NEW: add Design variant to FeatureTable

--- q2_types/feature_table/__init__.py
+++ q2_types/feature_table/__init__.py
@@ -12,10 +12,11 @@ from ._format import (BIOMV100Format, BIOMV210Format, BIOMV100DirFmt,
                       BIOMV210DirFmt)
 from ._type import (FeatureTable, Frequency, RelativeFrequency,
                     PresenceAbsence, Composition, Balance,
-                    PercentileNormalized)
+                    PercentileNormalized, Design)
 
 __all__ = ['BIOMV100Format', 'BIOMV100DirFmt', 'FeatureTable', 'Frequency',
            'RelativeFrequency', 'PresenceAbsence', 'BIOMV210Format',
-           'BIOMV210DirFmt', 'Composition', 'Balance', 'PercentileNormalized']
+           'BIOMV210DirFmt', 'Composition', 'Balance', 'PercentileNormalized',
+           'Design']
 
 importlib.import_module('q2_types.feature_table._transformer')
