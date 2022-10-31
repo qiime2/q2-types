26c4a2a17bb945cd5496aee23ea426248cf0903a
Greg Caporaso <greg.caporaso@nau.edu>
2022-10-31 07:35:14 -0700

    disabled skbio verification on parsing - too slow on real data

--- q2_types/multiplexed_sequences/_transformer.py
+++ q2_types/multiplexed_sequences/_transformer.py
@@ -25,7 +25,8 @@ def _1(df: MultiplexedFastaQualDirFmt) -> \
     # TODO: can I access the file name (forward.fastq.gz) from result
     # so I'm not duplicating that here?
     with open(result.path / 'forward.fastq.gz', 'wb') as fh:
-        for seq in skbio.io.read(seqs, qual=qual, format='fasta'):
+        for seq in skbio.io.read(seqs, qual=qual, format='fasta',
+                                 verify=False):
             seq.write(fh, format='fastq', variant='illumina1.8',
                       compression='gzip')
 
