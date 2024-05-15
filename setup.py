cbe2e4a017a5c41562963d31541111d1629eeafb
Hannah Hagen <hagenjohannah@gmail.com>
2024-05-15 09:55:06 -0700

    more testing setup.py

--- setup.py
+++ setup.py
@@ -28,15 +28,20 @@ setup(
         'q2_types': ['citations.bib'],
         'q2_types.tests': ['data/*'],
         'q2_types.distance_matrix.tests': ['data/*'],
-        'q2_types.feature_data.tests': ['data/*', 'data/taxonomy/*'],
+        'q2_types.feature_data.tests': ['data/*',
+                                        'data/taxonomy/*',
+                                        'data/emp_multiplexed/*',
+                                        'data/emp_multiplexed_single_end/*'],
         'q2_types.feature_map.tests': ['data/*'],
         'q2_types.feature_table.tests': ['data/*'],
         'q2_types.metadata.tests': ['data/*'],
-        'q2_types.multiplexed_sequences.tests': ['data/*'],
+        'q2_types.multiplexed_sequences.tests': [
+            'data/*',
+            'data/absolute_manifests_v2/*'
+            ],
         'q2_types.ordination.tests': ['data/*'],
         'q2_types.per_sample_sequences.tests':
             ['data/*',
-             'data/**',
              'data/paired_end_data/*',
              'data/paired_end_data_numeric/*',
              'data/single_end_data/*',
