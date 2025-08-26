df7c7ec5dbc7d0d0c215ce56aaa0bc4defdd5154
Liz Gehret <54517601+lizgehret@users.noreply.github.com>
2025-08-26 16:21:59 -0700

    Revert "Ensure the paired-end read pairs have an equal number of records (#366)"
    This reverts commit f604aa77caedc14f3e18aa2b5f1d5afb2b7c9563.

--- q2_types/per_sample_sequences/tests/test_formats.py
+++ q2_types/per_sample_sequences/tests/test_formats.py
@@ -150,32 +150,6 @@ class TestAbsoluteFastqManifestV2Formats(TestPluginBase):
                     'line 1.*absolute-filepath.*Human-Kneecap'):
                 fmt(manifest, mode='r').validate()
 
-    def test_paired_end_records_match(self):
-        '''
-        Tests that an error is raised when a pair of read mate files do not
-        have an equal number of records.
-        '''
-        unmatched_manifest = self.get_data_path(
-            'unmatched_paired_end/MANIFEST'
-        )
-        file_fwd_unmatch = self.get_data_path(
-            'unmatched_paired_end/sample-name-1_S1_L001_R1_001.fastq.gz'
-        )
-        file_rev_unmatch = self.get_data_path(
-            'unmatched_paired_end/sample-name-1_S1_L001_R2_001.fastq.gz'
-        )
-        manifest = self.template_manifest(
-           unmatched_manifest, {'r1': file_fwd_unmatch, 'r2': file_rev_unmatch}
-        )
-
-        for fmt in self.pe_formats:
-            with self.assertRaisesRegex(
-                ValidationError,
-                "A pair of paired-end files were found not to have the same "
-                "number of records"
-            ):
-                fmt(manifest, mode='r').validate()
-
 
 class TestAbsoluteFastqManifestFormats(TestPluginBase):
     package = 'q2_types.per_sample_sequences.tests'
@@ -446,32 +420,6 @@ class TestFormats(TestPluginBase):
 
         with self.assertRaisesRegex(ValidationError,
                                     'Duplicate.*Human-Kneecap'):
-            format.validate()
-
-    def test_casava_one_eight_slanepsample_dir_fmt_paired_match(self):
-        '''
-        Tests that an error is raised when a pair of read mate files do not
-        have an equal number of records.
-        '''
-        file_path_fwd = self.get_data_path(
-            'unmatched_paired_end/sample-name-1_S1_L001_R1_001.fastq.gz'
-        )
-        file_path_rev = self.get_data_path(
-            'unmatched_paired_end/sample-name-1_S1_L001_R2_001.fastq.gz'
-        )
-
-        shutil.copy(file_path_fwd, self.temp_dir.name)
-        shutil.copy(file_path_rev, self.temp_dir.name)
-
-        format = CasavaOneEightSingleLanePerSampleDirFmt(
-            self.temp_dir.name, mode='r'
-        )
-
-        with self.assertRaisesRegex(
-            ValidationError,
-            "A pair of paired-end files were found not to have the same "
-            "number of records"
-        ):
             format.validate()
 
     def test_miseq_demux_dir_fmt_validate_positive(self):
