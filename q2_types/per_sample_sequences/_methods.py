34e8feddb9ad6565bc22fd0ebbfc5b7ebd317ed7
cherman2 <chloeroseh@gmail.com>
2026-06-30 12:17:48 -0700

    linty

--- q2_types/per_sample_sequences/_methods.py
+++ q2_types/per_sample_sequences/_methods.py
@@ -38,7 +38,7 @@ def partition_sample_data_mags(
         all_samples = set(k for d in samples for k in d.keys())
         manifest = pd.read_csv(mags.path / "MANIFEST", index_col=None)
         manifest = \
-             manifest[manifest["sample-id"].astype("string").isin(all_samples)]
+            manifest[manifest["sample-id"].astype("string").isin(all_samples)]
         manifest.to_csv(result.path / "MANIFEST", index=False)
 
         for sample_dict in samples:
